bd183ed6c3ec78e5e947eae5386a3bc9e480c358
kim344 <dongju8907@gmail.com>
2022-07-01 15:25:45 +0900

    Base 작업 진행중

--- app/src/main/java/com/kim344/githubuserapisample/base/BaseViewModel.kt
+++ app/src/main/java/com/kim344/githubuserapisample/base/BaseViewModel.kt
@@ -1,4 +1,27 @@
 package com.kim344.githubuserapisample.base
 
-class BaseViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel: ViewModel() {
+
+    protected val compositeDisposable = CompositeDisposable()
+
+    private val _isLoading = MutableLiveData(false)
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    fun showLoading() {
+        _isLoading.value = true
+    }
+
+    fun hideLoading() {
+        _isLoading.value = false
+    }
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+        super.onCleared()
+    }
 }
